4a49549c8f4271770da4e6d4dd534259a6178d91
Dipam Patel <dp482@njit.edu>
2021-12-15 15:23:31 -0500

    chnages

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -6,13 +6,17 @@ import { LoginPageComponent } from './components/login-page/login-page.component
 import { ProductsDetailsComponent } from './components/products/products-details/products-details.component';
 import { ProductsComponent } from './components/products/products/products.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { AddNewProductComponent } from './components/add-new-product/add-new-product.component';
 
 const routes: Routes = [
   {path: '', component: HomepageComponent },
   {path: 'login', component: LoginPageComponent},
   {path: 'register', component: RegisterPageComponent},
   {path: 'products', component: ProductsComponent},
-  {path: 'products/:id', component: ProductsDetailsComponent}
+  {path: 'products/:id', component: ProductsDetailsComponent},
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'admin/add-new-product', component: AddNewProductComponent}
 ];
 
 @NgModule({
