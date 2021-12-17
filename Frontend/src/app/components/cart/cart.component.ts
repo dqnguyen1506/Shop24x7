b79d3704d634412eaef3d0ceb6037f19befd93c5
Dipam Patel <dp482@njit.edu>
2021-12-16 19:09:10 -0500

    compo Cart

--- Frontend/src/app/components/cart/cart.component.ts
+++ Frontend/src/app/components/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  productInfo: any; 
+
+  constructor(private _cartService : CartService, private route : Router) { }
 
   ngOnInit(): void {
+    this._cartService.getcart().subscribe(res =>{
+      this.productInfo = res.cart
+      console.log(this.productInfo)
+    })
   }
-
 }
